16de14ab13f8fffd6c4abd27a997d38021057253
Eren Güden <erenguden@Erens-Air.lan>
2024-05-23 01:57:05 -0700

    vd bg fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,6 @@ const HeroSection = () => {
           top: 0,
           left: 0,
           width: "100%",
-          height: "100%",
           objectFit: "cover",
           zIndex: -1,
         }}
