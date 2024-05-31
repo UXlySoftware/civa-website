3409297193fcdc3eecbb3ecb763b4830a0a4419f
Eren Güden <erenguden@Erens-Air.lan>
2024-05-31 15:50:56 -0700

    product page responsive

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -211,14 +211,58 @@ const ProductPage: React.FC = () => {
         <Box sx={styles.topBox}></Box>
         <Box sx={styles.featureBox}>
           <Typography variant="h5" sx={styles.featureHeader}>
-            CIVA OFFERS GOVERNMENT + COMMUNITY ENGAGEMENT — AND MORE.
+            CIVA OFFERS GOVERNMENT +<br /> COMMUNITY ENGAGEMENT — AND MORE.
           </Typography>
-          <Grid container>
-            <Grid item xs={12} sm={6}>
-              <List>
+          <Box
+            sx={{
+              ...styles.divider,
+              width: {
+                xs: "290px !important",
+                sm: "379px",
+                md: "1160px !important",
+                lg: "1160px !important",
+                xl: "1160px !important",
+              },
+              height: "10px !important",
+            }}
+          />
+          <Grid
+            container
+            sx={{
+              width: "92%",
+              margin: "0 auto 40px auto",
+            }}
+          >
+            <Grid item xs={12} sm={6} marginTop="10px">
+              <List
+                sx={{
+                  marginLeft: "15px",
+                  width: {
+                    xl: "100%",
+                    lg: "100%",
+                    md: "100%",
+                    sm: "100%",
+                    xs: "90%",
+                  },
+                }}
+              >
                 {features.map((feature, index) => (
                   <ListItem key={index} sx={styles.listItem}>
-                    <ListItemText primary={feature.left} />
+                    <ListItemText
+                      primary={feature.left}
+                      primaryTypographyProps={{
+                        fontSize: {
+                          xl: "19px",
+                          lg: "19px",
+                          md: "19px",
+                          sm: "19px",
+                          xs: "13px",
+                        },
+                        fontFamily: "Inter",
+                        fontWeight: 600,
+                        marginTop: "12px",
+                      }}
+                    />
                     <ListItemIcon sx={styles.checkIconContainer}>
                       <CheckIcon sx={styles.checkIcon} />
                     </ListItemIcon>
@@ -226,11 +270,42 @@ const ProductPage: React.FC = () => {
                 ))}
               </List>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <List>
+            <Grid item xs={12} sm={6} marginTop="22px">
+              <List
+                sx={{
+                  marginLeft: {
+                    xl: "45px",
+                    lg: "45px",
+                    md: "45px",
+                    sm: "45px",
+                    xs: "13px",
+                  },
+                  width: {
+                    xl: "100%",
+                    lg: "100%",
+                    md: "100%",
+                    sm: "100%",
+                    xs: "90%",
+                  },
+                }}
+              >
                 {features.map((feature, index) => (
                   <ListItem key={index} sx={styles.listItem}>
-                    <ListItemText primary={feature.right} />
+                    <ListItemText
+                      primary={feature.right}
+                      primaryTypographyProps={{
+                        fontSize: {
+                          xl: "19px",
+                          lg: "19px",
+                          md: "19px",
+                          sm: "19px",
+                          xs: "13px",
+                        },
+                        fontFamily: "Inter",
+                        fontWeight: 600,
+                        marginBottom: "7px",
+                      }}
+                    />
                     <ListItemIcon sx={styles.checkIconContainer}>
                       <CheckIcon sx={styles.checkIcon} />
                     </ListItemIcon>
@@ -242,9 +317,37 @@ const ProductPage: React.FC = () => {
         </Box>
         <Paper elevation={3} sx={styles.bottomBox}>
           <Typography sx={styles.bottomText}>
-            CIVA offers more than traditional government and community
-            engagement tools, combining the benefits of both into a single
-            integrated platform.
+            <Typography
+              sx={{
+                fontSize: { xl: "28px", lg: "28px", xs: "20px" },
+                fontWeight: 900,
+                display: "inline",
+                fontFamily: "Public Sans",
+              }}
+            >
+              CIVA
+            </Typography>{" "}
+            <Typography
+              sx={{
+                fontFamily: "Inter",
+                fontWeight: 400,
+                fontSize: { xl: "28px", lg: "28px", xs: "20px" },
+                display: "inline",
+              }}
+            >
+              offers more than traditional government and community engagement
+              tools,
+            </Typography>{" "}
+            <Typography
+              sx={{
+                fontWeight: 900,
+                fontSize: { xs: "20px", sm: "28px" },
+                display: "inline",
+                fontFamily: "Public Sans",
+              }}
+            >
+              combining the benefits of both into a single integrated platform.
+            </Typography>
           </Typography>
         </Paper>
 
@@ -466,12 +569,13 @@ const styles = {
       xl: "52px",
     },
     textAlign: "center",
+    marginBottom: "12px",
   },
   divider: {
     textAlign: "center",
-    marginBottom: "20px",
+    marginBottom: { xl: "20px" },
     width: {
-      xs: "379px",
+      xs: "349px",
       sm: "379px",
       md: "660px",
       lg: "660px",
@@ -479,32 +583,43 @@ const styles = {
     },
     margin: "auto",
     backgroundColor: "#FFC000",
-    height: "10px",
+    height: "16px",
   },
   featureBox: {
+    border: "1px solid red",
+    margin: "0 auto 40px auto",
+    width: {
+      xl: "1283px",
+      lg: "1283px",
+      md: "736px",
+      sm: "736px",
+      xs: "349px",
+    },
+    height: { xl: "530px", lg: "530px", md: "322px", sm: "322px", xs: "auto" },
     backgroundColor: "#000000",
     color: "#FFFFFF",
-    padding: "20px",
     borderRadius: "5px",
-    marginBottom: "20px",
-    marginTop: "40px",
-    marginLeft: "40px",
-    marginRight: "40px",
+    // marginBottom: "20px",
+    marginTop: { xl: "100px", lg: "130px", md: "80px", sm: "80px", xs: "60px" },
+    // marginLeft: "40px",
+    // marginRight: "40px",
     position: "relative",
     zIndex: 2,
   },
   featureHeader: {
-    marginBottom: "10px",
-    borderBottom: "10px solid #FFC000",
-    paddingBottom: "10px",
-    fontWeight: "800",
-    fontSize: {
-      xs: "1rem",
-      sm: "1rem",
-      md: "1.5rem",
-      lg: "1.5rem",
-      xl: "1.5rem",
+    width: { xl: "100%", xs: "75%" },
+    // marginBottom: "10px",
+    padding: {
+      xl: "30px 60px",
+      lg: "30px 60px",
+      md: "30px 60px",
+      sm: "30px 60px",
+      xs: "30px 30px 12px 30px",
     },
+    lineHeight: { xl: "30px", lg: "36px", xs: "22px" },
+    fontFamily: "Public Sans",
+    fontWeight: 900,
+    fontSize: { xl: "28px", lg: "28px", md: "17px", sm: "17px", xs: "17px" },
   },
   listItem: {
     borderBottom: "1px solid #FFFFFF",
@@ -526,75 +641,84 @@ const styles = {
   },
   checkIcon: {
     color: "#FFD700",
+    stroke: "#FFD700",
+    strokeWidth: 3,
+    fontSize: "23px",
   },
   topBox: {
     backgroundColor: "#2E86AB",
     width: {
-      xs: "45%",
+      xs: "226px",
       sm: "45%",
       md: "30%",
-      lg: "15%",
-      xl: "15%",
+      lg: "429px",
+      xl: "439px",
     },
-    height: "200px",
+    height: { xl: "305px", lg: "305px", md: "305px", sm: "305px", xs: "185px" },
     alignContent: "flex-end",
     position: "absolute",
     zIndex: 1,
     right: {
-      xs: "20px",
+      xs: "5px",
       sm: "20px",
       md: "40px",
-      lg: "60px",
-      xl: "60px",
+      lg: "35px",
+      xl: "270px",
     },
-    marginTop: "20px",
+    marginTop: { xl: "55px", lg: "85px", xs: "40px" },
   },
   bottomBox: {
+    textAlign: "left",
     backgroundColor: "#FFC000",
-    padding: "20px",
-    textAlign: "center",
-    marginTop: "20px",
+    borderRadius: 0,
+    // padding: "0px 20px 0px 20px",
+    // marginTop: "20px",
     width: {
-      xs: "80%",
+      xs: "374px",
       sm: "80%",
       md: "60%",
-      lg: "50%",
-      xl: "50%",
+      lg: "1102px",
+      xl: "1102px",
     },
     height: {
-      xs: "180px",
+      xs: "302px",
       sm: "180px",
       md: "300px",
-      lg: "280px",
-      xl: "280px",
+      lg: "580px",
+      xl: "585px",
     },
     alignContent: "flex-end",
     top: {
-      xs: "-100px",
+      xs: "-140px",
       sm: "-100px",
       md: "-150px",
-      lg: "-200px",
-      xl: "-200px",
+      lg: "-375px",
+      xl: "-400px",
     },
     position: "relative",
     zIndex: 1,
     marginLeft: {
-      xs: "5%",
+      xs: "2%",
       sm: "5%",
       md: "2%",
-      lg: "0",
-      xl: "0",
+      lg: "-20px",
+      xl: "200px",
     },
   },
   bottomText: {
+    width: { xl: "70%", lg: "70%", md: "80%", sm: "80%", xs: "94%" },
+    marginLeft: { xl: "65px", lg: "65px", md: "65px", sm: "65px", xs: "17px" },
+    marginBottom: "55px",
+    marginTop: { xs: "120px", sm: 0 },
     color: "#000000",
     fontSize: {
-      xs: "1rem",
+      xs: "20px",
       sm: "1rem",
       md: "1rem",
-      lg: "1.4rem",
-      xl: "1.4rem",
+      lg: "28px",
+      xl: "28px",
     },
+    lineHeight: { xl: "41px", lg: "41px", md: "41px", sm: "41px", xs: "22px" },
   },
 };
 
